c62928f3542df5a4b9a52c8ccf64616a48ef2c9c
KimPuro <puro@protonmail.ch>
2024-04-19 12:15:31 +0900

    style : use next/head

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,9 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
       <html lang="en">
-      <head>
-        <link rel="icon" href="/images/icon/favicon.ico" type="image/x-icon"/>
-      </head>
+      <Head>
+          <link rel="icon" href="/images/icon/favicon.ico" type="image/x-icon"/>
+      </Head>
       <body className={inter.className}>{children}</body>
 
 
